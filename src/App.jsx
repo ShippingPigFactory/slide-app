da9bdc14374e6eede56c6ba02bb6f605f80c5418
catshanddev <catshanddev@gmail.com>
2025-12-19 08:53:29 +0900

    必要のないページを削除

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
 // src/App.jsx
 import React, { useState } from 'react';
 import './App.css';
-import Logo from './assets/steraLogo.jpg';
 
 // 作成したスライドファイルをインポート
 import Slide1 from './components/slides/Slide1';
 import Slide2 from './components/slides/Slide2';
-import Slide3 from './components/slides/Slide3';
-import Slide4 from './components/slides/Slide4';
 import Slide5 from './components/slides/Slide5';
-import Slide6 from './components/slides/Slide6';
 import Slide7 from './components/slides/Slide7';
 import Slide8 from './components/slides/Slide8';
-import Slide9 from './components/slides/Slide9';
-import Slide10 from './components/slides/Slide10';
-import Slide11 from './components/slides/Slide11';
-import Slide12 from './components/slides/Slide12';
 import Slide13 from './components/slides/Slide13';
 import Slide14 from './components/slides/Slide14';
 import Slide15 from './components/slides/Slide15';
-import Slide16 from './components/slides/Slide16';
 import Slide17 from './components/slides/Slide17';
 import Slide18 from './components/slides/Slide18';
-import Slide19 from './components/slides/Slide19';
 import Slide20 from './components/slides/Slide20';
-import Slide21 from './components/slides/Slide21';
 import Slide22 from './components/slides/Slide22';
 import Slide23 from './components/slides/Slide23';
-import Slide24 from './components/slides/Slide24';
 import Slide25 from './components/slides/Slide25';
 
 function App() {
@@ -41,28 +29,17 @@ function App() {
   const slides = [
     <Slide1 key="slide1" />,
     <Slide2 key="slide2" />,
-    // <Slide3 key="slide3" />,
-    // <Slide4 key="slide4" />,
     <Slide5 key="slide5" />,
-    // <Slide6 key="slide6" />,
     <Slide7 key="slide7" />,
     <Slide8 key="slide8" />,
-    // <Slide9 key="slide9" />,
-    // <Slide10 key="slide10" />,
-    // <Slide11 key="slide11" />,
-    // <Slide12 key="slide12" />,
     <Slide13 key="slide13" />,
     <Slide14 key="slide14" />,
     <Slide15 key="slide15" />,
-    // <Slide16 key="slide16" />,
     <Slide17 key="slide17" />,
     <Slide18 key="slide18" />,
-    // <Slide19 key="slide19" />,
     <Slide20 key="slide20" />,
-    // <Slide21 key="slide21" />,
     <Slide22 key="slide22" />,
     <Slide23 key="slide23" />,
-    // <Slide24 key="slide24" />,
     <Slide25 key="slide25" />,
   ];
 
